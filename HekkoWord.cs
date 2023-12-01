8300533c26c1ab226b1de44e4419af1c619d23b4
Aryna1 <152649468+Aryna1@users.noreply.github.com>
2023-12-01 20:05:06 +0100

    Add files via upload

---
+++ HekkoWord.cs
@@ -0,0 +1,19 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HekkoWord : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        //imprimimos la entrada del game 
+        print("Hola :)");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
